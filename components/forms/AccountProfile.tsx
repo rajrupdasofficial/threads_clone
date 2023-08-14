8e8a28165a45845c1706f5d38ce81cf81e3370b8
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-14 12:33:12 +0530

    Changes include
    Chnages inlude new form style and design
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,22 @@
 "use client"
 
+import { useForm } from 'react-hook-form';
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
+import * as z from "zod"
+import Image from 'next/image';
+ 
 
 interface Props{
     user:{
@@ -14,10 +31,61 @@ interface Props{
 }
 
 const AccountProfile = ({ user, btnTitle }: Props ) => {
+    const form = useForm({
+        resolver: zodResolver(UserValidation),
+        defaultValues:{
+            profile_photos:'',
+            name: '',
+            username: '',
+            bio: '',
+        }
+    });
+    
+      
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        console.log(values)
+      }
     return (
-        <div>
-            AccoutProfile
-        </div>
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
+          <FormField
+            control={form.control}
+            name="profile_photo"
+            render={({ field }) => (
+              <FormItem className='flex items-center gap-4'>
+                <FormLabel className='account-from_image-label'>
+                  {field.value ?(
+                  <Image 
+                  src={field.value}
+                  alt="profile photo"
+                  width={96}
+                  height={96}
+                  priority
+                  className='rounded-full object-contain'
+                   />
+                ) : (
+                  <Image 
+                  src="/assets/profile.svg"
+                  alt="profile photo"
+                  width={24}
+                  height={24}
+                  className='object-contain'
+                   />
+                )}
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     )
 }
 
