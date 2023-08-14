48fdb8051f3af8eb2acc00aff8972c23a59b0cb9
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-14 11:01:39 +0530

    Right sidebar layout fixed
    Right sidebar layout hasbeen fixed
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,7 +6,7 @@ function RightSidebar(){
 				<h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
 			</div>
 			<div className="flex flex-1 flex-col justify-start">
-				<h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+				<h3 className="text-heading4-medium text-light-1">Suggested User</h3>
 			</div>
 		</section>
 	)
